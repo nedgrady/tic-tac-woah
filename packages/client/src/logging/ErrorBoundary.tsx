ac0414b9fe8a82e1e7d02346cc4870afc99870e9
Ned Grady <gradyned@gmail.com>
2023-11-11 13:24:15 +0000

    Add an error boundary

---
+++ packages/client/src/logging/ErrorBoundary.tsx
@@ -0,0 +1,34 @@
+import { ApplicationInsights } from "@microsoft/applicationinsights-web"
+import { AppInsightsErrorBoundary, ReactPlugin } from "@microsoft/applicationinsights-react-js"
+
+export interface ReactPropsWithChildren {
+	children: React.ReactElement
+}
+
+var reactPlugin = new ReactPlugin()
+var appInsights = new ApplicationInsights({
+	config: {
+		instrumentationKey: "691cf8f7-d5ef-45df-a5ff-385d9429be4b",
+		extensions: [reactPlugin],
+	},
+})
+
+appInsights.addTelemetryInitializer(item => {
+	if (!item.tags) item.tags = []
+	if (!item.ext) item.ext = []
+
+	item.tags["ai.cloud.role"] = "tic-tac-woah.client"
+	item.tags["ai.cloud.roleInstance"] = window.location.hostname
+	item.ext["tic-tac-woah.source"] = "default"
+	return true
+})
+
+appInsights.loadAppInsights()
+
+export default function ErrorBoundary({ children }: ReactPropsWithChildren) {
+	return (
+		<AppInsightsErrorBoundary onError={() => <p>Something went wrong...</p>} appInsights={reactPlugin}>
+			{children}
+		</AppInsightsErrorBoundary>
+	)
+}
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import { Provider } from "react-redux"
 import { store } from "./redux/store"
-import loadApplicationInsights from "./logging/applicationInsights"
-
-loadApplicationInsights()
+import ErrorBoundary from "./logging/ErrorBoundary"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
+		<ErrorBoundary>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</ErrorBoundary>
 	</React.StrictMode>
 )
