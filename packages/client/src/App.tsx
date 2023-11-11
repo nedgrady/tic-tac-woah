2275f5b3e53bd85006521f1ebf50d12c861edf18
Ned Grady <gradyned@gmail.com>
2023-11-11 12:53:56 +0000

    Move out app insights into its own file.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -4,12 +4,9 @@ import { io } from "socket.io-client"
 import { GameStartDtoSchema, MoveDtoSchema } from "types"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { useAppDispatch } from "./redux/hooks"
-// import { newMove } from "./redux/boardSlice"
 import { CoordinatesDto } from "types"
-import { ApplicationInsights } from "@microsoft/applicationinsights-web"
-import { ReactPlugin, withAITracking } from "@microsoft/applicationinsights-react-js"
 import { Game } from "./Game"
-import { Coordinate, Move, newMove, startGame } from "./redux/gameSlice"
+import { Coordinate, newMove, startGame } from "./redux/gameSlice"
 
 const webSocketUrl = `${import.meta.env.VITE_WEBSOCKET_URL}:${import.meta.env.VITE_WEBSOCKET_PORT}`
 
@@ -18,26 +15,6 @@ export const socket = io(webSocketUrl, {
 })
 
 const queryClient = new QueryClient()
-
-var reactPlugin = new ReactPlugin()
-var appInsights = new ApplicationInsights({
-	config: {
-		instrumentationKey: "691cf8f7-d5ef-45df-a5ff-385d9429be4b",
-		extensions: [reactPlugin],
-	},
-})
-
-appInsights.addTelemetryInitializer(item => {
-	if (!item.tags) item.tags = []
-	if (!item.ext) item.ext = []
-
-	item.tags["ai.cloud.role"] = "tic-tac-woah.client"
-	item.tags["ai.cloud.roleInstance"] = window.location.hostname
-	item.ext["tic-tac-woah.source"] = "default"
-	return true
-})
-
-appInsights.loadAppInsights()
 
 function App() {
 	const dispatch = useAppDispatch()
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import { Provider } from "react-redux"
 import { store } from "./redux/store"
+import loadApplicationInsights from "./logging/applicationInsights"
+
+loadApplicationInsights()
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
