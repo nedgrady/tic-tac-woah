4da3ffe3dfe0be9d78f9f910485c9a025bb9494d
Ned Grady <gradyned@gmail.com>
2023-11-12 17:22:52 +0000

    Lots of rejigging with the goal of making games more flexible

--- packages/server/src/Game.test.ts
+++ packages/server/src/Game.test.ts
@@ -3,6 +3,8 @@ import { Game } from "./Game"
 import { Move } from "./Move"
 import { Participant } from "./Participant"
 import { faker } from "@faker-js/faker"
+import Coordinates from "./Coordinates"
+import _ from "lodash"
 
 interface GameRules {
 	gridSize?: number
@@ -29,42 +31,70 @@ it("New games start with an empty set of moves", () => {
 	expect(game.moves()).toHaveLength(0)
 })
 
+type PlacementSpecification = (Participant | Empty)[][]
+type Empty = "."
+
+// TODO - ensure this apples ALL moves regardless of order
+function makeMoves(placementDefinitions: PlacementSpecification) {
+	const placementDefinitionsClone = placementDefinitions.map(placementSpecifications => [...placementSpecifications])
+
+	while (_.flatMap(placementDefinitionsClone).some(participant => participant !== ".")) {
+		placementDefinitionsClone.forEach((row, rowIndex) => {
+			row.forEach((participant, columnIndex) => {
+				//console.log("Applying move", { rowIndex, columnIndex, participant })
+				if (participant === ".") return
+
+				participant.makeMove({ x: columnIndex, y: rowIndex })
+				placementDefinitionsClone[rowIndex][columnIndex] = "."
+			})
+		})
+	}
+}
+
 it("Participant one making a move is captured", () => {
 	const {
 		game,
-		participants: [participantOne],
+		participants: [p1],
 	} = gameWithParticipants()
 
-	participantOne.makeMove({ x: 0, y: 0 })
+	p1.makeMove({ x: 0, y: 0 })
 
-	const expectedMove = { placement: { x: 0, y: 0 }, mover: participantOne }
+	const expectedMove = { placement: { x: 0, y: 0 }, mover: p1 }
 	expect(game.moves()[0]).toEqual<Move>(expectedMove)
 })
 
 it("Participant two making a move is captured", () => {
 	const {
 		game,
-		participants: [participantOne, participantTwo],
+		participants: [p1, p2],
 	} = gameWithParticipants()
 
-	participantOne.makeMove({ x: 0, y: 0 })
-	participantTwo.makeMove({ x: 1, y: 1 })
+	makeMoves([
+		[p1, "."],
+		[".", "."],
+	])
 
-	const expectedMove = { placement: { x: 1, y: 1 }, mover: participantTwo }
+	p2.makeMove({ x: 1, y: 1 })
+
+	const expectedMove = { placement: { x: 1, y: 1 }, mover: p2 }
 	expect(game.moves()[1]).toEqual<Move>(expectedMove)
 })
 
 it("Participant three making a move is captured", () => {
 	const {
 		game,
-		participants: [participantOne, participantTwo, participantThree],
+		participants: [p1, p2, p3],
 	} = gameWithParticipants()
 
-	participantOne.makeMove({ x: 0, y: 0 })
-	participantTwo.makeMove({ x: 1, y: 1 })
-	participantThree.makeMove({ x: 2, y: 2 })
+	makeMoves([
+		[p1, ".", "."],
+		[".", p2, "."],
+		[".", ".", "."],
+	])
 
-	const expectedMove = { placement: { x: 2, y: 2 }, mover: participantThree }
+	p3.makeMove({ x: 2, y: 2 })
+
+	const expectedMove = { placement: { x: 2, y: 2 }, mover: p3 }
 	expect(game.moves()[2]).toEqual<Move>(expectedMove)
 })
 
@@ -123,13 +153,16 @@ describe("Out of order moves", () => {
 	it("Participant one makes a turn out of order", () => {
 		const {
 			game,
-			participants: [participantOne],
+			participants: [p1],
 		} = gameWithParticipants()
 
-		participantOne.makeMove({ x: 0, y: 0 })
+		makeMoves([
+			[p1, "."],
+			[".", "."],
+		])
 
 		const outOfTurnMove = { x: 1, y: 1 }
-		participantOne.makeMove(outOfTurnMove)
+		p1.makeMove(outOfTurnMove)
 
 		expect(game.moves()).toHaveLength(1)
 	})
@@ -137,14 +170,16 @@ describe("Out of order moves", () => {
 	it("Participant two makes a turn out of order", () => {
 		const {
 			game,
-			participants: [participantOne, participantTwo],
+			participants: [p1, p2],
 		} = gameWithParticipants()
 
-		participantOne.makeMove({ x: 0, y: 0 })
-		participantTwo.makeMove({ x: 1, y: 1 })
+		makeMoves([
+			[p1, "."],
+			[".", p2],
+		])
 
 		const outOfTurnMove = { x: 2, y: 2 }
-		participantTwo.makeMove(outOfTurnMove)
+		p2.makeMove(outOfTurnMove)
 
 		expect(game.moves()).toHaveLength(2)
 	})
@@ -152,14 +187,20 @@ describe("Out of order moves", () => {
 	it("Participant two makes a second turn out of order", () => {
 		const {
 			game,
-			participants: [participantOne, participantTwo, participantThree],
+			participants: [p1, p2, p3],
 		} = gameWithParticipants()
 
-		participantOne.makeMove({ x: 0, y: 0 })
-		participantTwo.makeMove({ x: 1, y: 1 })
-		participantThree.makeMove({ x: 2, y: 2 })
+		// participantOne.makeMove({ x: 0, y: 0 })
+		// participantTwo.makeMove({ x: 1, y: 1 })
+		// participantThree.makeMove({ x: 2, y: 2 })
 
-		participantTwo.makeMove({ x: 3, y: 3 })
+		makeMoves([
+			[p1, ".", "."],
+			[".", p2, "."],
+			[".", ".", p3],
+		])
+
+		p2.makeMove({ x: 3, y: 3 })
 
 		expect(game.moves()).toHaveLength(3)
 	})
@@ -194,7 +235,7 @@ describe("Making an out of bound move", () => {
 		const {
 			game,
 			participants: [participantOne],
-		} = gameWithParticipants({ gridSize: 20 })
+		} = gameWithParticipants({ gridSize: gameSize })
 
 		participantOne.makeMove(coordinates)
 
@@ -206,17 +247,99 @@ it("Making a move in a taken square", () => {
 	// Ensure player can't move in a square that's already been taken
 	const {
 		game,
-		participants: [participantOne, participantTwo],
+		participants: [p1, p2],
 	} = gameWithParticipants()
 
-	participantOne.makeMove({ x: 0, y: 0 })
-	participantTwo.makeMove({ x: 0, y: 0 })
+	makeMoves([
+		[p1, "."],
+		[".", "."],
+	])
+
+	p2.makeMove({ x: 0, y: 0 })
 
 	expect(game.moves()).toHaveLength(1)
 })
 
-describe("Winning a game", () => {
-	it("Is triggered when player one wins vertically", () => {
+describe("Winning a game vertically", () => {
+	it("Is triggered when player one in the top left", () => {
+		const {
+			game,
+			participants: [p1, p2],
+		} = gameWithParticipants({
+			gridSize: 20,
+			consecutiveTarget: 3,
+			participantCount: 2,
+		})
+
+		const mockWinListener = vitest.fn()
+		game.onWin(mockWinListener)
+
+		makeMoves([
+			[p1, ".", p2],
+			[p1, ".", p2],
+			[".", ".", "."],
+		])
+
+		p1.makeMove({ x: 0, y: 2 })
+
+		expect(mockWinListener).toHaveBeenCalledOnce()
+	})
+
+	it("Is not triggered in the same scenario as above but a higher target...", () => {
+		const {
+			game,
+			participants: [p1, p2],
+		} = gameWithParticipants({
+			gridSize: 20,
+			consecutiveTarget: 4,
+			participantCount: 2,
+		})
+
+		const mockWinListener = vitest.fn()
+		game.onWin(mockWinListener)
+
+		makeMoves([
+			[p1, ".", p2],
+			[p1, ".", p2],
+			[".", ".", "."],
+		])
+
+		p1.makeMove({ x: 0, y: 2 })
+
+		expect(mockWinListener).not.toHaveBeenCalledOnce()
+	})
+
+	it("Is triggered when player two wins in the top left", () => {
+		const {
+			game,
+			participants: [p1, p2],
+		} = gameWithParticipants({
+			gridSize: 20,
+			consecutiveTarget: 3,
+			participantCount: 2,
+		})
+
+		const mockWinListener = vitest.fn()
+		game.onWin(mockWinListener)
+
+		p1.makeMove({ x: 9, y: 9 })
+		p2.makeMove({ x: 0, y: 0 })
+		p1.makeMove({ x: 9, y: 8 })
+		p2.makeMove({ x: 0, y: 1 })
+		p1.makeMove({ x: 6, y: 6 })
+
+		// makeMoves([
+		// 	[p2, ".", p1],
+		// 	[p2, ".", p1],
+		// 	[".", p1, "."],
+		// ])
+
+		p2.makeMove({ x: 0, y: 2 })
+
+		expect(mockWinListener).toHaveBeenCalledOnce()
+	})
+
+	it("Is not triggered when a player makes non-consecutive moves in the same column", () => {
 		const {
 			game,
 			participants: [participantOne, participantTwo],
@@ -230,34 +353,73 @@ describe("Winning a game", () => {
 		game.onWin(mockWinListener)
 
 		participantOne.makeMove({ x: 0, y: 0 })
-		participantTwo.makeMove({ x: 9, y: 9 })
-		participantOne.makeMove({ x: 0, y: 1 })
-		participantTwo.makeMove({ x: 8, y: 8 })
+		participantTwo.makeMove({ x: 0, y: 1 })
 		participantOne.makeMove({ x: 0, y: 2 })
+		participantTwo.makeMove({ x: 0, y: 3 })
+		participantOne.makeMove({ x: 0, y: 4 })
 
-		expect(mockWinListener).toHaveBeenCalledOnce()
+		expect(mockWinListener).not.toHaveBeenCalledOnce()
 	})
 
-	it("Is not triggered in the same scenario as above but a higher target...", () => {
+	it("Is triggered when player one wins in the bottom left", () => {
 		const {
 			game,
 			participants: [participantOne, participantTwo],
 		} = gameWithParticipants({
 			gridSize: 20,
-			consecutiveTarget: 4,
+			consecutiveTarget: 3,
 			participantCount: 2,
 		})
 
 		const mockWinListener = vitest.fn()
 		game.onWin(mockWinListener)
 
-		participantOne.makeMove({ x: 0, y: 0 })
+		participantOne.makeMove({ x: 0, y: 19 })
 		participantTwo.makeMove({ x: 9, y: 9 })
-		participantOne.makeMove({ x: 0, y: 1 })
+		participantOne.makeMove({ x: 0, y: 18 })
 		participantTwo.makeMove({ x: 8, y: 8 })
-		participantOne.makeMove({ x: 0, y: 2 })
+		participantOne.makeMove({ x: 0, y: 17 })
 
-		expect(mockWinListener).not.toHaveBeenCalledOnce()
+		expect(mockWinListener).toHaveBeenCalledOnce()
+	})
+
+	it("Is triggered when player one wins an arbitrarily large game", () => {
+		const gridSize = faker.number.int({ min: 10, max: 100 })
+
+		const {
+			game,
+			participants: [participantOne, participantTwo],
+		} = gameWithParticipants({
+			gridSize,
+			consecutiveTarget: gridSize,
+			participantCount: 2,
+		})
+
+		const mockWinListener = vitest.fn()
+		game.onWin(mockWinListener)
+
+		const nonWinningCoordinateIndices = Array.from({ length: gridSize - 1 }, (_, index) => index)
+
+		const participantOneCoordinatesForNonWinningMoves = nonWinningCoordinateIndices.map((_, index) => ({
+			x: 0,
+			y: index,
+		}))
+
+		const participantTwoCoordinates = nonWinningCoordinateIndices.map((_, index) => ({
+			x: 1,
+			y: index,
+		}))
+
+		nonWinningCoordinateIndices.forEach(index => {
+			participantOne.makeMove(participantOneCoordinatesForNonWinningMoves[index])
+			participantTwo.makeMove(participantTwoCoordinates[index])
+		})
+
+		const participantOneCoordinatesForWinningMove = { x: 0, y: gridSize - 1 }
+
+		participantOne.makeMove(participantOneCoordinatesForWinningMove)
+
+		expect(mockWinListener).toHaveBeenCalledOnce()
 	})
 
 	it("Is not triggered when player two makes their first non-terminal move", () => {
@@ -297,4 +459,57 @@ describe("Winning a game", () => {
 
 		expect(mockWinListener).not.toHaveBeenCalled()
 	})
+
+	it("Is triggered when player one wins in an arbitrary column", () => {
+		const gridSize = 20
+		const winningColumnIndex = faker.number.int({ min: 1, max: gridSize - 1 })
+
+		const {
+			game,
+			participants: [participantOne, participantTwo],
+		} = gameWithParticipants({
+			gridSize,
+			consecutiveTarget: 3,
+			participantCount: 2,
+		})
+
+		const mockWinListener = vitest.fn()
+		game.onWin(mockWinListener)
+
+		participantOne.makeMove({ x: winningColumnIndex, y: 0 })
+		participantTwo.makeMove({ x: 0, y: 0 })
+		participantOne.makeMove({ x: winningColumnIndex, y: 1 })
+		participantTwo.makeMove({ x: 0, y: 1 })
+		participantOne.makeMove({ x: winningColumnIndex, y: 2 })
+
+		expect(mockWinListener).toHaveBeenCalledOnce()
+	})
+
+	// it("Is triggered when player one wins after making moves in various columns", () => {
+	// 	const {
+	// 		game,
+	// 		participants: [participantOne, participantTwo],
+	// 	} = gameWithParticipants({
+	// 		gridSize: 20,
+	// 		consecutiveTarget: 3,
+	// 		participantCount: 2,
+	// 	})
+
+	// 	const mockWinListener = vitest.fn()
+	// 	game.onWin(mockWinListener)
+
+	// 	participantOne.makeMove({ x: 0, y: 0 })
+	// 	participantTwo.makeMove({ x: 1, y: 0 })
+	// 	participantOne.makeMove({ x: 0, y: 1 })
+	// 	participantTwo.makeMove({ x: 1, y: 1 })
+
+	// 	participantOne.makeMove({ x: 18, y: 0 })
+	// 	participantTwo.makeMove({ x: 19, y: 0 })
+	// 	participantOne.makeMove({ x: 18, y: 1 })
+	// 	participantTwo.makeMove({ x: 19, y: 1 })
+
+	// 	participantOne.makeMove({ x: 19, y: 2 })
+
+	// 	expect(mockWinListener).toHaveBeenCalledOnce()
+	// })
 })
--- packages/server/src/Game.ts
+++ packages/server/src/Game.ts
@@ -2,6 +2,44 @@ import { Mock } from "vitest"
 import { Move } from "./Move"
 import { Participant } from "./Participant"
 import { EventEmitter } from "events"
+import _ from "lodash"
+
+// TODO should this return something else e.g. a string?
+type GameRuleFunction = (newMove: Move, gameState: GameState, gameConfiguration: GameConfiguration) => boolean
+
+interface GameConfiguration {
+	readonly boardSize: number
+	readonly target: number
+
+	//readonly rules: readonly GameRuleFunction[]
+}
+
+interface GameState {
+	readonly moves: readonly Move[]
+	readonly participants: readonly Participant[]
+}
+
+function moveMustBeWithinTheBoard(newMove: Move, gameState: GameState, gameConfiguration: GameConfiguration) {
+	return (
+		newMove.placement.x >= 0 &&
+		newMove.placement.y >= 0 &&
+		newMove.placement.x < gameConfiguration.boardSize &&
+		newMove.placement.y < gameConfiguration.boardSize
+	)
+}
+
+function moveMustBeMadeByTheCorrectPlayer(newMove: Move, gameState: GameState, gameConfiguration: GameConfiguration) {
+	return newMove.mover === gameState.participants[gameState.moves.length % gameState.participants.length]
+}
+
+function moveMustBeMadeIntoAFreeSquare(newMove: Move, gameState: GameState, gameConfiguration: GameConfiguration) {
+	return !gameState.moves.some(
+		existingMove =>
+			existingMove.placement.x === newMove.placement.x && existingMove.placement.y === newMove.placement.y
+	)
+}
+
+const standardRules = [moveMustBeWithinTheBoard, moveMustBeMadeByTheCorrectPlayer, moveMustBeMadeIntoAFreeSquare]
 
 export class Game {
 	readonly #target: number
@@ -9,6 +47,7 @@ export class Game {
 	readonly #participants: readonly Participant[]
 	readonly #emitter: EventEmitter = new EventEmitter()
 	readonly #boardSize: number
+	readonly #rules: readonly GameRuleFunction[]
 
 	onWin(listener: () => void) {
 		this.#emitter.on("Winning Move", listener)
@@ -26,33 +65,38 @@ export class Game {
 	}
 
 	submitMove(newMove: Move) {
-		if (newMove.placement.x < 0 || newMove.placement.y < 0) return
-
-		if (newMove.placement.x >= this.#boardSize || newMove.placement.y >= this.#boardSize) return
-
-		// Ensure the move is made by the correct participant
-		if (newMove.mover !== this.#participants[this.#movesReal.length % this.#participants.length]) {
-			return
+		const gameConfiguration: GameConfiguration = {
+			boardSize: this.#boardSize,
+			target: this.#target,
 		}
 
-		// Ensure the square is not already taken
-		if (
-			this.#movesReal.some(
-				existingMove =>
-					existingMove.placement.x === newMove.placement.x && existingMove.placement.y === newMove.placement.y
-			)
-		) {
-			return
+		const gameState: GameState = {
+			moves: this.#movesReal,
+			participants: this.#participants,
+		}
+
+		for (let rule of this.#rules) {
+			if (!rule(newMove, gameState, gameConfiguration)) {
+				return
+			}
 		}
 
 		this.#movesReal.push(newMove)
 		this.#emitter.emit("Move", newMove)
 
-		const playerOnesMoves = this.#movesReal.filter(move => move.mover === this.#participants[0])
+		const movesByCurrentPlayer = this.#movesReal
+			.filter(move => move.mover === newMove.mover)
+			.sort((move1, move2) => move1.placement.y - move2.placement.y)
 
-		const topLeftThreeVerticals = playerOnesMoves.filter(move => move.placement.x === 0)
-		if (topLeftThreeVerticals.length === 3 && this.#target === 3) {
-			this.#emitter.emit("Winning Move")
+		if (movesByCurrentPlayer.length < this.#target) return
+
+		for (let placementsChunk of overlappingChunks(movesByCurrentPlayer, this.#target)) {
+			if (
+				placementsChunk[placementsChunk.length - 1].placement.y - placementsChunk[0].placement.y ===
+				this.#target - 1
+			) {
+				this.#emitter.emit("Winning Move")
+			}
 		}
 	}
 
@@ -60,10 +104,32 @@ export class Game {
 		return this.#movesReal
 	}
 
-	constructor(participants: readonly Participant[], boardSize: number = 20, target: number = 9999) {
-		participants.forEach(participant => (participant.game = this))
+	constructor(
+		participants: readonly Participant[],
+		boardSize: number = 20,
+		target: number = 9999,
+		rules: readonly GameRuleFunction[] = standardRules
+	) {
+		participants.forEach((participant, index) => {
+			participant.game = this
+			;(participant as any).idx = index
+		})
 		this.#participants = participants
 		this.#boardSize = boardSize
 		this.#target = target
+		this.#rules = rules
 	}
+}
+
+function overlappingChunks<TItem>(array: TItem[], chunkSize: number) {
+	if (chunkSize <= 0 || chunkSize > array.length) {
+		throw new Error("Invalid chunk size")
+	}
+
+	const result = []
+	for (let i = 0; i <= array.length - chunkSize; i++) {
+		result.push(array.slice(i, i + chunkSize))
+	}
+
+	return result
 }
