56f335c596401aef25322e342ed3ce1024dcd763
Ned Grady <gradyned@gmail.com>
2024-04-12 01:53:18 +0100

    Move the last test!

---
+++ packages/server/src/auth/whenDisconnecting/whenTheUserKeepsAnotherConnectionOpen.test.ts
@@ -0,0 +1,39 @@
+import { faker } from "@faker-js/faker"
+import { ActiveUser } from "TicTacWoahSocketServer"
+import { identifyAllSocketsAsTheSameUser, removeConnectionFromActiveUser } from "auth/socketIdentificationStrategies"
+import { StartAndConnectLifetime } from "ticTacWoahTest"
+import { beforeAll, expect, describe, it, vi } from "vitest"
+
+describe("it", () => {
+	const activeUser: ActiveUser = {
+		connections: new Set(),
+		uniqueIdentifier: "Some active user",
+	}
+
+	const testLifetime = new StartAndConnectLifetime(
+		server => server.use(identifyAllSocketsAsTheSameUser(activeUser)).use(removeConnectionFromActiveUser),
+		2
+	)
+
+	beforeAll(async () => {
+		await testLifetime.start()
+
+		testLifetime.clientSocket.disconnect()
+
+		await vi.waitFor(async () => {
+			const activeSockets = await testLifetime.serverIo.fetchSockets()
+			expect(activeSockets).toHaveLength(1)
+		})
+
+		return testLifetime.done
+	})
+
+	it("Removes a connection from the active user", async () => {
+		expect(activeUser.connections).toHaveLength(1)
+	})
+
+	it("Leaves the open connection against the active user", async () => {
+		const activeSockets = await testLifetime.serverIo.fetchSockets()
+		expect(activeUser.connections).toContain(activeSockets[0])
+	})
+})
