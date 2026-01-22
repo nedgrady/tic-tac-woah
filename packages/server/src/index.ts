11bad8822ea19291bd8914488c03cf2ce9bba62a
Ned Grady <gradyned@gmail.com>
2026-01-22 11:46:21 +0000

    Types

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,12 +1,12 @@
 export * from "./aiAgents/gemini/GeminiAiAgent"
-export { CreateGameOptions, Game } from "./domain/Game"
+export { type CreateGameOptions, type Game } from "./domain/Game"
 export { anyParticipantMayMoveNext } from "./domain/moveOrderRules/support/anyParticipantMayMoveNext"
 export { moveMustBeMadeIntoAFreeSquare } from "./domain/gameRules/gameRules"
 export {
 	winByConsecutiveDiagonalPlacements,
 	winByConsecutiveVerticalPlacements,
 } from "./domain/winConditions/winConditions"
-export { makeMoves, PlacementSpecification } from "./domain/gameTestHelpers"
-export { default as Coordinates } from "./domain/Coordinates"
+export { makeMoves, type PlacementSpecification } from "./domain/gameTestHelpers"
+export { type default as Coordinates } from "./domain/Coordinates"
 export { RandomlyMovingAiParticipantFactory } from "./aiAgents/RandomlyMovingAiParticipantFactory"
 export { AiParticipant } from "./aiAgents/AiParticipant"
